e671771688ada8b3a99e2b32e5f30e44654b109e
tarascom <gonzo.quant@gmail.com>
2019-03-03 11:14:28 +0200

    3 files for home task # added

---
+++ taras_naumenko/Pizza.js
@@ -0,0 +1,23 @@
+function Pizza(toppings, bortik, size) {
+    this.toppings = toppings;
+    this.bortik = bortik;
+    this.size = size;
+    this.makePizza = function() {
+       if (this.bortik){
+   console.log(this.size + " Pizza with bortik" + " and with toppings: " + this.toppings);
+    }else {
+   console.log(this.size + " Pizza without bortik" + " and with toppings: " + this.toppings);
+    }
+    }
+    this.packPizza = function() {
+   console.log("Pizza packed into pack: " + this.size);
+    }
+   }
+   
+   var myPizza1 = new Pizza(['onion', 'bekon', 'tomato'], true, 'big');
+   var myPizza2 = new Pizza(['cheeze', 'olive'], false, 'small');
+   
+   myPizza1.makePizza();
+   myPizza1.packPizza();
+   myPizza2.makePizza();
+   myPizza2.packPizza();
