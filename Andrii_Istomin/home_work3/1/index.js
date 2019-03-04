262d392e8d33a9958a8118e6647f478dee0a535d
DeBasser <debasser1911@gmail.com>
2019-03-04 18:51:03 +0200

    home task 3

---
+++ Andrii_Istomin/home_work3/1/index.js
@@ -0,0 +1,25 @@
+class Pizza {
+    constructor(toppings, bortik, size){
+        this.toppings = toppings;
+        this.bortik = bortik;
+        this.size = size;
+    }
+    makePizza(){
+        return (this.size + " Pizza with " + this.bortik + " and with toppings: " + this.toppings)
+    }
+    packPizza(box){
+        return("Pizza packed into " + box + " box")
+    }
+}
+
+var pizza = new Pizza(["sause", "mozarella", "cheddar"], "kolbaska", "L")
+var pizza1 = new Pizza(["sause", "peperoni", "tomato"], "", "XL")
+var pizza2 = new Pizza(["sause", "mozarella", "cheddar"], "kolbaska", "XXL")
+
+console.log(pizza.makePizza())
+console.log(pizza1.makePizza())
+console.log(pizza2.makePizza())
+console.log(pizza.packPizza("BIG"))
+
+
+
---
+++ Andrii_Istomin/home_work3/2/index.js
@@ -0,0 +1,5 @@
+function applyAll(func, ...args) {
+  return func(...args);
+}
+
+console.log(applyAll(Math.max, 25, 155, 266, 264464, 123134846887654));
