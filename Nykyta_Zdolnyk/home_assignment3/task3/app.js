3d0dd7dbc6d069fb0b840648e1d7f04437323ddd
Nykyta Zdolnyk <zdolniknikita@gmail.com>
2019-03-04 12:13:58 +0200

    Added home_assignment 3

---
+++ Nykyta_Zdolnyk/home_assignment3/task3/app.js
@@ -0,0 +1,31 @@
+function viewObjectKeys() {
+    for (var key in this) {
+        console.log(key + ": " + this[key]);
+    }
+}
+
+function getValueByKeys(key1, key2) {
+    console.log(this[key1] + ", " + this[key2]);
+}
+
+var obj1 = {
+    key1: "ключ 1",
+    key2: "ключ 2",
+    key3: "ключ 3",
+    someValue1: 30
+};
+
+var obj2 = {
+    key4: "ключ 4",
+    key5: "ключ 5",
+    key6: "ключ 6",
+    someValue2: 50
+};
+
+viewObjectKeys.call(obj1);
+
+getValueByKeys.apply(obj2,['key4','someValue2']);
+
+var f = getValueByKeys.bind(obj2)
+f('key5', 'key6');
+
