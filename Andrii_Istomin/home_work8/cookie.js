e946aec3c55ee5c25cce70efde28170b273f0ffd
DeBasser <debasser1911@gmail.com>
2019-03-21 18:41:47 +0200

    home work 8

---
+++ Andrii_Istomin/home_work8/cookie.js
@@ -0,0 +1,10 @@
+var cookie = document.cookie;
+
+function getCookie(){
+  if (cookie != "") {
+    document.getElementById("main").appendChild(document.createElement("input"))
+    
+  }
+
+}
+getCookie();
