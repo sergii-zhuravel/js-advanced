02a5ad90c00748f22f4f3a91336d4442e5d3c036
zaynch01 <A.Zaynchukovskyy@kostal.com>
2019-03-26 17:08:23 +0200

    task + ex2

---
+++ Alexandr_Zaynchukovskyy/hometask2/cars.js
@@ -0,0 +1,30 @@
+function CarWithAutomaticTransmission(wheels, color) {
+  this.wheels = wheels || 4;
+  this.color = color || 'white';
+  this.engineOn = false;
+  this.transmission = 0;
+
+  this.switchEngine = function () {
+    if (this.engineOn) {
+      console.log("Turning engine off");
+      this.engineOn = false;
+    } else {
+      console.log("Turning engine on");
+      this.engineOn = true;
+    }
+  };
+  this.drive = function () {
+    this.switchEngine();
+    console.log("Im driving. I'm a " + this.color + " car");
+  }
+}
+
+
+var ford = new CarWithAutomaticTransmission(undefined, 'red');
+ford.drive();
+
+var kamaz = new CarWithAutomaticTransmission(6, 'grey');
+kamaz.drive();
+
+var samokat = new CarWithAutomaticTransmission(2, 'metal');
+samokat.drive();
