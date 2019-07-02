a0ef0aeb8bc270a56a290c4b6082c674ed6f8f44
boyar4uk <Boyar4uk@gmail.com>
2019-07-02 19:51:56 +0300

    asd

--- Nikita_Boyar4uk/Практика 2/script.js
+++ Nikita_Boyar4uk/Практика 2/script.js
@@ -5,10 +5,10 @@
 
 var user = prompt('Введи число');
 
-if (user % 2 == 0){
-    console.log('Четнное');
+if (user % 2){
+    console.log(' Не Четнное');
 } else if (isNaN(user)) {
     console.log('необходимо ввести число');
 }  else {
-    console.log('НЕ четное');
+    console.log('Четное');
 }
