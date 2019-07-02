622bf1c0a2c94493cea83876c11c6739ab6a58ba
Nikita <sabaoth@ukr.net>
2019-07-02 20:56:21 +0300

    game finnished

--- Nikita_Kornienko/HW_1/first task/js/main.js
+++ Nikita_Kornienko/HW_1/first task/js/main.js
@@ -2,9 +2,15 @@ let numberMade = document.getElementById('numberMade');
 let boardInput = document.getElementById('boardInput').value;
 let boardGuess = document.getElementById('boardGuess');
 let boardText = document.getElementById('boardText');
+let restartBtn = document.getElementById('restartBtn');
 let result = document.getElementById('result');
+let score = document.getElementById('score');
 let modal = document.querySelector('.modal');
 let number;
+let gameScores = {
+    Player: 0,
+    Computer: 0
+}
 
 
 // Makes up a number from 1 to 10
@@ -31,9 +37,19 @@ function showGuess () {
         let inside = document.getElementById('boardInside');
         if(boardInput == number) {
             inside.classList.add('green');
+            gameScores.Player ++;
+            score.innerHTML = `
+            <p>Player: ${gameScores.Player}</p>
+            <p>Computer: ${gameScores.Computer}</p>
+            `;
         } else {
             inside.classList.add('red');
             showResult(number);
+            gameScores.Computer ++;
+            score.innerHTML = `
+            <p>Player: ${gameScores.Player}</p>
+            <p>Computer: ${gameScores.Computer}</p>
+            `;
         }
     }, 1000);
     
@@ -42,10 +58,37 @@ function showGuess () {
 function showResult (number) {
     result.innerHTML = `
     <h1>You've lost!</h1>
-    <p>Computer Chose <strong>${number}</strong></p>
+    <p>Computer Chose <span id="loseNumber"><strong>${number}</strong></span></p>
     `;
+    let loseNumber = document.getElementById('loseNumber');
+    loseNumber.classList.add('red');
     modal.style.display = 'block';
 }
 
+// Close modal 
+
+function closeModal (e) {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+// Restart Game
+
+function restartGame () {
+    gameScores.Player = 0;
+    gameScores.Computer = 0;
+    score.innerHTML = `
+        <p>Player: 0</p>
+        <p>Computer: 0</p>
+        `;
+    boardText.innerHTML = `
+    <span class="board__text--inside" id="boardInside"></span>
+    `;
+    boardInput = '';
+}
+
+window.addEventListener('click', closeModal);
 numberMade.addEventListener('click', createNumber);
 boardGuess.addEventListener('click', showGuess);
+restartBtn.addEventListener('click', restartGame);
