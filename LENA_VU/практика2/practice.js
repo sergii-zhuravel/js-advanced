d5e4b01212a41fe22c5511e88fc31b65debcfdb7
unknown <vulena13@gmail.com>
2019-07-11 17:52:58 +0300

    hw2 update

--- LENA_VU/практика2/practice.js
+++ LENA_VU/практика2/practice.js
@@ -8,6 +8,50 @@ function sum() {
 
 }
 
+function sum1() {
+	// создает массив аргументов функции sum1
+	var arr = [].slice.call(arguments);
+}
+
+function adder(n) {
+	function (m) {
+
+	return n+m;}
+	 
+	};
+var myAdder=adder(5);// function(m){return 5+m;}  
+
+function makeCounter(n) {
+
+return function (){
+	return ++n; 
+	
+}
+}
+
+function proto(obj){
+
+	var chain= typeof(obj.__proto__)+" ";
+
+	while(!(obj==null)){
+		obj=obj.__proto__; chain+=" > " +typeof obj;
+	}
+    return chain;
+}
+
+function prototype(obj){
+
+	if (obj == null) return "null";
+
+}
+
+
+
+
+
+
+
+
 
 // var res=0;
 // res = prompt("input any number");
